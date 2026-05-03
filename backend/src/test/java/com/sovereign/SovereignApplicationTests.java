13b767d19dc3966026175ee782ebdc68e594dbb7
ntefera530 <nathan.tefera@outlook.com>
2026-05-02 17:27:53 -0700

    inital commit

---
+++ backend/src/test/java/com/sovereign/SovereignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovereign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SovereignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
