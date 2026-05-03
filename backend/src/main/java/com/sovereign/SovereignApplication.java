768425376ee0cac629ba2a06a8d5483384ccb331
ntefera530 <nathan.tefera@outlook.com>
2026-05-02 18:41:13 -0700

    configure postgres connection with env

--- backend/src/main/java/com/sovereign/SovereignApplication.java
+++ backend/src/main/java/com/sovereign/SovereignApplication.java
@@ -1,11 +1,10 @@
-package com.sovereign.sovereign;
+package com.sovereign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SovereignApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SovereignApplication.class, args);
 	}
